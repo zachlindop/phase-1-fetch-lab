70b9922b1a1aff8969fd5aa1b3f54c9ddc238dac
zachlindop <85177168+zachlindop@users.noreply.github.com>
2021-07-24 12:37:52 -0700

    Done with assignment

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then(resp => resp.json())
+  .then(json => renderBooks(json)); 
 }
+
+//function fetchBooks() {
+//  fetch('https://anapioficeandfire.com/api/books')
+//  .then(res => res.json())
+//  .then(json => renderBooks(json))
+//  return renderBooks
+//}
 
 function renderBooks(books) {
   const main = document.querySelector('main');
